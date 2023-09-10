8d6d39c3651f8c201c0f93bea3c31dda1093080c
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-10 16:42:34 -0700

    GuessMyNumber: Implemented Handling Events

--- 01 - Guess My Number/script.js
+++ 01 - Guess My Number/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// document.querySelector('.message').textContent = '🎉 Correct Number';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
+
+// const guessInput = document.querySelector('.guess').value;
+
+// Random Number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+// Handling Click Events
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  } else if ((guess = secretNumber)) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  }
+});
