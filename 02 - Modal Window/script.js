d1ea1d14cffa74a7794402a8a25fc8489d8b5f67
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-12 19:05:15 -0700

    ModalWindow: restart

--- 02 - Modal Window/script.js
+++ 02 - Modal Window/script.js
@@ -6,28 +6,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// A function that closes our modals by adding CSS class to it
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
+console.log(btnsOpenModal);
 
-// A function that shows our modals by removing CSS class to it
-const showModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
-
-//When modals are clicked show Modal
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', showModal);
+  console.log(btnsOpenModal[i].textContent);
 }
-
-//Close modals when clicked
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-//Close modal when we hit escape
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
-});
