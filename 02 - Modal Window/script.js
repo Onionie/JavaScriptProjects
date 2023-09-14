9884075634baac578c8b00d0e7cc68026d47d3c0
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-13 20:16:33 -0700

    ModalWindow: Added event listener to each Show modal buttons

--- 02 - Modal Window/script.js
+++ 02 - Modal Window/script.js
@@ -9,5 +9,8 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
