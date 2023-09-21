44dbdce11b074423b5befd63a0fd9371c276a11f
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-20 18:50:30 -0700

    PigGame: Selected starting variables

--- 03 - Pig-Game/script.js
+++ 03 - Pig-Game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Selecting Elements
+const score1Element = document.querySelector('#score--0');
+const score2Element = document.querySelector('#score--1');
+const diceElement = document.getElementsByClassName('dice');
+
+// Starting Conditions
+score1Element.textContent = 0;
+score2Element.textContent = 0;
+diceElement.classList.add('hidden');
