f8b9c82a8ffa4dc3450f47de57d4c9e7e423f718
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-21 17:26:06 -0700

    PigGame: Added rolling dice functionality

--- 03 - Pig-Game/script.js
+++ 03 - Pig-Game/script.js
@@ -3,9 +3,31 @@
 // Selecting Elements
 const score1Element = document.querySelector('#score--0');
 const score2Element = document.querySelector('#score--1');
-const diceElement = document.getElementsByClassName('dice');
+const current1Element = document.getElementById('current--0');
+const current2Element = document.getElementById('current--1');
+
+const diceElement = document.querySelector('.dice');
+const btnNewGame = document.querySelector('btn--new');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting Conditions
 score1Element.textContent = 0;
 score2Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+let currentScore = 0;
+
+// Roll dice functionality
+const rollDice = () => {
+  const diceNumber = Math.trunc(Math.random() * 6 + 1);
+  diceElement.classList.remove('hidden');
+  diceElement.src = `./images/dice-${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    current1Element.textContent = currentScore;
+  }
+};
+
+btnRollDice.addEventListener('click', rollDice);
