fe43b25d189eb6250f4aa99f935acc5e07196b81
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-09-21 17:50:44 -0700

    PigGame: Implemented switching the active player

--- 03 - Pig-Game/script.js
+++ 03 - Pig-Game/script.js
@@ -5,7 +5,8 @@ const score1Element = document.querySelector('#score--0');
 const score2Element = document.querySelector('#score--1');
 const current1Element = document.getElementById('current--0');
 const current2Element = document.getElementById('current--1');
-
+const player1Element = document.querySelector('.player--0');
+const player2Element = document.querySelector('.player--1');
 const diceElement = document.querySelector('.dice');
 const btnNewGame = document.querySelector('btn--new');
 const btnRollDice = document.querySelector('.btn--roll');
@@ -16,7 +17,9 @@ score1Element.textContent = 0;
 score2Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+let scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Roll dice functionality
 const rollDice = () => {
@@ -26,7 +29,14 @@ const rollDice = () => {
 
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current1Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer == 0 ? 1 : 0;
+    player1Element.classList.toggle('player--active');
+    player2Element.classList.toggle('player--active');
   }
 };
 
