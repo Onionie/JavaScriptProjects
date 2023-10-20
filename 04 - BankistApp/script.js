32f0b25f71343a652169782e672aa790dc166c8e
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-10-20 13:06:59 -0700

    BankistApp: Creating usernames

--- 04 - BankistApp/script.js
+++ 04 - BankistApp/script.js
@@ -79,3 +79,15 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const createUsername = function (accs) {
+  accs.forEach(function (acc) {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+createUsername(accounts);
