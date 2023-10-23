3400e65d6561c4352b3d199e0cfda1798c2b5189
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-10-22 18:49:34 -0700

    BankistApp: Solved Challegne #3

--- 04 - BankistApp/notes.js
+++ 04 - BankistApp/notes.js
@@ -228,3 +228,22 @@ const totalDeposits = movements
   }, 0);
 
 console.log(totalDeposits);
+
+console.log('');
+console.log('--- CHALLENGE 3 ---');
+/*
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+const calcAverageHumanAge1 = function (arr) {
+  let dogInHumanYears = arr
+    .map(dogAge => (dogAge <= 2 ? 2 * dogAge : 16 + dogAge * 4))
+    .filter(dogAge => dogAge >= 18)
+    .reduce((acc, curr, i, arr) => acc + curr / arr.length, 0);
+  return dogInHumanYears;
+};
+
+console.log(calcAverageHumanAge1([5, 2, 4, 1, 15, 8, 3]));
