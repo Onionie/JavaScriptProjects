64d3a3594c1ba772f42eea8ff00ebf96fd26fe2c
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-10-24 17:10:21 -0700

    BankistApp: Numeric Separator

--- 04 - BankistApp/notes2.js
+++ 04 - BankistApp/notes2.js
@@ -5,7 +5,7 @@
 console.log('--- LECTURE ---');
 console.log('--- CONVERTING & CHECKING NUMBERS ---');
 
-console.log(23 = 23.0); // true
+console.log(23 == 23.0); // true
 
 // Conversion
 console.log(Number('23')); // 23 type === number
@@ -24,14 +24,14 @@ console.log('');
 console.log('--- MATH & ROUNDING ---');
 
 console.log(Math.sqrt(25)); // 5;
-console.log(25 ** (1/2)); // 5;
-console.log(8 ** (1/3)); // 2; cubic root
+console.log(25 ** (1 / 2)); // 5;
+console.log(8 ** (1 / 3)); // 2; cubic root
 
 console.log(Math.max(5, 18, 23, 11, 2)); //23
 console.log(Math.max(5, 18, '23', 11, 2)); // 23
 console.log(Math.max(5, 18, 23, 11, 2)); // NaN
 
-console.log(Math.trunc(Math.random() * 6 +1)); // random numbers from 1-6
+console.log(Math.trunc(Math.random() * 6 + 1)); // random numbers from 1-6
 
 // Rounding Integers
 console.log(Math.trunc(23.3)); //23; removes all decimals
@@ -62,7 +62,19 @@ console.log(8 / 3); // 2.6666;
 console.log(6 % 2); // 0; even
 console.log(7 % 2); // 0; odd
 
-const isEven = n => n%2 ===0;
+const isEven = n => n % 2 === 0;
 console.log(isEven(8)); // true
 console.log(isEven(23)); // false
 console.log(isEven(514)); // true
+
+console.log('');
+console.log('--- NUMERIC SEPARATORS ---');
+
+// 287,460,000,000
+// JS can use underscore as comma or period as separators
+const diamater = 287_460_000_000;
+console.log(diamater);
+
+const priceCents = 345_99;
+
+console.log(Number('230_000')); // NaN
