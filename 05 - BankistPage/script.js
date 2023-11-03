811482edaf3e4537d912e392b2841059bfda138a
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-11-03 14:51:02 -0700

    BankistPage: Selectind, Creating and Deleting Elements

--- 05 - BankistPage/script.js
+++ 05 - BankistPage/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,38 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/*****************************************
+ *                                       *
+ *                  NOTES                *
+ *                                       *
+ *****************************************/
+const header = document.querySelector('.header');
+
+// CREATING AND INSERTING ELEMENTS
+// .insertAdjacentHTML
+
+// .createElement
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality.';
+message.innerHTML =
+  'We use cookies for improved analytics. <button class ="btn btn--close-cookie"> Got It! </button>';
+
+header.prepend(message); // First-child
+// header.append(message); // Last-child
+
+// Need to clone an element
+// header.append(message.cloneNode(true));
+
+// as a sibling - before/after
+// header.before(message);
+// header.after(message);
+
+// DELETE ELEMENTS
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove();
+    message.parentElement.removeChild(message);
+  });
