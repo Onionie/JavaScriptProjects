74cf4a6560b8cee135902c06937fa645df4363a5
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-11-09 17:38:54 -0800

    BankistPage: Sticky navigation using Intersection Observer API

--- 05 - BankistPage/script.js
+++ 05 - BankistPage/script.js
@@ -121,13 +121,26 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
-// Sticky navigation
-const initialCoords = section1.getBoundingClientRect();
+// Sticky navigation: Intersection Observer API
 
-window.addEventListener('scroll', function (e) {
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+headerObserver.observe(header);
 
 //
 /*****************************************
