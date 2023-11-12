83ff5b173d7bb98aadb187c4bcd3bca3e086824b
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2023-11-12 13:51:00 -0800

    BankistPage: Slider buttons working

--- 05 - BankistPage/script.js
+++ 05 - BankistPage/script.js
@@ -198,19 +198,42 @@ const btnRight = document.querySelector('.slider__btn--right');
 const dotContainer = document.querySelector('.dots');
 
 let curSlide = 0;
+const maxSlide = slides.length;
 
-slides.forEach((s, i) => {
-  s.style.transform = `translateX(${100 * i}%)`;
-});
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
 
-// Next Slide
-btnRight.addEventListener('click', function () {
-  curSlide++;
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
 
-  slides.forEach((s, i) => {
-    s.style.transform = `translateX(${100 * (i - curSlide)}%)`;
-  });
-});
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+const init = function () {
+  goToSlide(0);
+};
+init();
+
+// Event handlers
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
 
 //
 /*****************************************
